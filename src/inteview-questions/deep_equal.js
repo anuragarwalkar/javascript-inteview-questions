1a2d9548c74c474c74f80d7aaa59329ebd486d4d
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-10-17 20:33:16 +0530

    Added

--- src/inteview-questions/deep_equal.js
+++ src/inteview-questions/deep_equal.js
@@ -9,16 +9,37 @@ function deepEqual(value1, value2) {
         }
 
         for (let i = 0; i < value1.length; i++) {
-            deepEqual(value1[i], value2[i]);
+            return deepEqual(value1[i], value2[i]);
         }
 
     }
 
+    const valueOneKeys = Object.keys(value1);
+    const valueTwoKeys = Object.keys(value2);
+    let allKeysValid = true;
+
+    if (valueOneKeys.length === valueTwoKeys.length) {
+        for (let i = 0; i < valueOneKeys.length; i++) {
+            const val = deepEqual(valueOneKeys, valueOneKeys);
+            !val && (allKeysValid = val)
+        }
+
+        if (!allKeysValid) {
+            return false;
+        }
+
+        for (let i = 0; i < valueOneKeys.length; i++) {
+            return deepEqual(value1[valueOneKeys[i]], value2[valueOneKeys[i]]);
+        }
+    } else {
+        return false;
+
+    }
 
     return true;
 }
 
 
-const result = deepEqual([], []);
+const result = deepEqual({ parent: { child: { name: 'anurag' } } }, { parent: { child: { name: 'anurag' } } });
 
 console.log('result:', result);
