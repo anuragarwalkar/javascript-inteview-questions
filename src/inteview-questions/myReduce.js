c00efb63f466aa2f235d2ed544c8bc29a7bc4ac9
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-12-04 10:08:16 +0530

    adde

---
+++ src/inteview-questions/myReduce.js
@@ -0,0 +1,25 @@
+const myArray = [];
+
+Array.prototype.myReduce = myReduce;
+
+function myReduce(callback, initialValue) {
+  let currentValue = initialValue || this[0];
+
+  if (this.length === 0) {
+    if (initialValue == null) {
+      return new Error("Please provide initial value in case empty array");
+    } else {
+      return result;
+    }
+  }
+
+  for (let index = 1; index < this.length; index++) {
+    currentValue = callback(currentValue, this[index], index, this);
+  }
+
+  return currentValue;
+}
+
+const result = myArray.myReduce((a, b) => a + b);
+
+console.log("result:", result);
