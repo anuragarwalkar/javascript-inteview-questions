2ef64f2c36f189ff5fd314bcccf84358332cf0db
Anurag Arwalkar <anuragarwalkar@gmail.com>
2023-02-01 10:07:37 +0530

    Update my_interview_questions.js

--- src/inteview-questions/my_interview_questions.js
+++ src/inteview-questions/my_interview_questions.js
@@ -5,6 +5,16 @@ function foo() {
   var a = 20;
 }
 
+const b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => console.log(b[i]), 1000);
+}
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(() => console.log(b[i]), 1000);
+}
+
 foo();
 
 // Tell sequence of the console logs
