b80c30be9e03a9eb23d84a49b002827b388878ae
Anurag Arwalkar <anuragarwalkar@gmail.com>
2023-12-05 14:42:20 +0530

    Update interview-question-medium.js

--- src/interivew-questions/interview-question-medium.js
+++ src/interivew-questions/interview-question-medium.js
@@ -15,18 +15,12 @@ function groupBy(inputArray) {
 }
 
 // output will be this
-const output = [
-    {
+const output = {
         21: [{ name: 'Harry', age: 21 },
         { name: 'Ron', age: 21 },],
-    },
-    {
-        20: [{ name: 'Draco', age: 20 },],
-    },
-    {
-        18: [{ name: 'Hermoine', age: 18 }],
-    },
-];
+         20: [{ name: 'Draco', age: 20 },],
+          18: [{ name: 'Hermoine', age: 18 }],
+    }
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
