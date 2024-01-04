f8a98d6e3034d0e3d4f6f1c398f4d15772c50459
Anurag Arwalkar <anuragarwalkar@gmail.com>
2024-01-04 22:42:36 +0530

    Update interview-questions-hard.js

--- src/interivew-questions/interview-questions-hard.js
+++ src/interivew-questions/interview-questions-hard.js
@@ -38,26 +38,75 @@ console.log(flaternObj(deeplyNestedMap));
 const allValues = [1, [1, [[9], [2], [3]]]];
 
 const removeNestedDuplicates = (values) => {
-    if (!Array.isArray(values)) {
-        return values;
-    }
-
-    const resultArray = []
-
-    for (const item of values) {
-        const value = removeNestedDuplicates(item);
-
-        if (Array.isArray(value)) {
-            for (const inner of value) {
-                if (!resultArray.includes(inner)) {
-                    resultArray.push(inner);
-                }
-            }
-        } else if (item && !resultArray.includes(value)) {
-            resultArray.push(value);
-        }
-    }
-
-    return resultArray;
+    
 }
+
+removeNestedDiplicates(allValues) // [1, 9, 2, 3]
+
+// Solution -----------------------------------------------------------
+// if (!Array.isArray(values)) {
+//         return values;
+//     }
+
+//     const resultArray = []
+
+//     for (const item of values) {
+//         const value = removeNestedDuplicates(item);
+
+//         if (Array.isArray(value)) {
+//             for (const inner of value) {
+//                 if (!resultArray.includes(inner)) {
+//                     resultArray.push(inner);
+//                 }
+//             }
+//         } else if (item && !resultArray.includes(value)) {
+//             resultArray.push(value);
+//         }
+//     }
+
+//     return resultArray;
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const obj = {
+  a: 1,
+  b: {
+    c: "Hello World",
+    d: 2,
+    f: false,
+    e: {
+     f: {
+       g: -4,
+      },
+    },
+    h: "Good Night Moon",
+  },
+};
+
+function deepFilter(object, filterCallback) {
+// mutate original object
+
+}
+
+//filter's in-place
+deepFilter(obj, (s) => typeof s === "string");
+
+console.log(JSON.stringify(obj, null, 2));
+
+// Solution -------------------------------------------------------------------
+// if(typeof object === 'object') {
+//     for(let key in object) {
+//         if(!filterCallback(object[key]) && typeof object[key] !== 'object') {
+//             delete object[key];
+//         }else {
+//             deepFilter(object[key], filterCallback)
+//         }
+
+//         if(typeof object[key] === 'object' && Object.keys(object[key]).length === 0) {
+//            delete object[key];
+//        }
+//     }
+// }
+
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
