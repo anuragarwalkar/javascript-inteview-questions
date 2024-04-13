2a7bde38e09ff0fb42f6a630f6eca5a6be4c3763
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-04-13 12:09:13 +0530

    update

---
+++ src/interivew-questions/easy.js
@@ -0,0 +1,21 @@
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// Group By
+let people = [
+  { name: "Harry", age: 21 },
+  { name: "Hermoine", age: 18 },
+  { name: "Ron", age: 21 },
+  { name: "Draco", age: 20 },
+];
+
+function groupBy(inputArray, key) {}
+
+// Example groupBy(people, 'age');
+// {
+//   21: [
+//     { name: "Harry", age: 21 },
+//     { name: "Ron", age: 21 },
+//   ],
+//   20: [{ name: "Draco", age: 20 }],
+//   18: [{ name: "Hermoine", age: 18 }],
+// }
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
