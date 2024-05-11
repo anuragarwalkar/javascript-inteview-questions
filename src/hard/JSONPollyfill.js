d3b0065b856c4f98abc85097525200bcdd84b574
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-05-11 18:36:52 +0530

    updated

--- src/hard/JSONPollyfill.js
+++ src/hard/JSONPollyfill.js
@@ -1,5 +1,4 @@
 // Serialization/ Deserialization
-
 class MyJSON {
   static parse(inputString) {
     let rootObject = {};
@@ -24,13 +23,39 @@ class MyJSON {
       return value;
     }
 
+    function splitByQuama(inputString) {
+      const finalResult = [];
+      const stack = [];
+      let start = 0;
+
+      for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] === "[" || inputString[i] === "{") {
+          stack.push("[");
+        } else if (inputString[i] === "]" || inputString[i] === "}") {
+          stack.pop();
+        } else if (stack.length === 0 && inputString[i] === ",") {
+          finalResult.push(inputString.slice(start, i));
+          start = i + 1;
+        }
+      }
+
+      finalResult.push(inputString.slice(start, inputString.length));
+      return finalResult;
+    }
+
     function recurssion(currentString, currentValue) {
-      const strItems = currentString.slice(1, -1).split(",");
+      const strItems = splitByQuama(currentString.slice(1, -1));
 
       if (Array.isArray(currentValue)) {
-        // for (let value of strItems) {
-        //   currentValue.push(recurssion(value, currentValue));
-        // }
+        for (let value of strItems) {
+          if (value.startsWith("{")) {
+            currentValue.push(recurssion(value, {}));
+          } else if (value.startsWith("[")) {
+            currentValue.push(recurssion(value, []));
+          } else {
+            currentValue.push(parseValueByString(value));
+          }
+        }
       } else {
         for (let keyValuePair of strItems) {
           const [key, value] = keyValuePair
@@ -102,18 +127,16 @@ class MyJSON {
 }
 
 const bigNestedObject = {
-  // id: 1,
+  id: 1,
   name: "Big Nested Object",
-  // details: {
-  //   description: "This is a deeply nested object for demonstration.",
-  // },
-  items: [1, 2, 3],
+  details: {
+    description: "This is a deeply nested object for demonstration.",
+  },
+  items: [{ values: [1, 2, 3] }],
 };
 
-console.log(MyJSON.stringify(bigNestedObject));
-console.log("-----------");
-// console.log("OEM", JSON.parse(JSON.stringify(bigNestedObject)));
-console.log("-----------");
-// console.log("OES", MyJSON.parse(MyJSON.stringify(bigNestedObject)));
-console.log("-----------");
-// {"id":1,"name":"Big Nested Object","metadata":{"createdBy":"Admin","createdAt":"2024-05-06","version":1}}
+console.log("String", MyJSON.stringify(bigNestedObject));
+console.log("----------- >");
+console.log("OEM", JSON.parse(JSON.stringify(bigNestedObject)));
+console.log("----------- >");
+console.log("OES", MyJSON.parse(MyJSON.stringify(bigNestedObject)));
